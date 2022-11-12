045fef305aadf2c23d4d299b560a6aeba417f09e
hoathi1 <118059493+hoathi1@users.noreply.github.com>
2022-11-12 20:34:14 +0700

    Add files via upload

---
+++ sayhentai/src/home.js
@@ -0,0 +1,8 @@
+function execute() {
+    return Response.success([
+        { title: "Cập Nhật", input: "https://sayhentai.us", script: "gen.js" },
+        { title: "Manhwa", input: "https://sayhentai.us/genre/manhwa", script: "gen.js" },
+        { title: "Manga", input: "https://sayhentai.us/genre/manga", script: "gen.js" },
+        { title: "Manhua", input: "https://sayhentai.us/genre/manhua", script: "gen.js" },
+    ]);
+}
